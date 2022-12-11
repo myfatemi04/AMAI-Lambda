b3125e7438547003fdb185f7a618098a2c3468a5
Michael Fatemi <myfatemi04@gmail.com>
2022-12-11 13:47:56 -0500

    make it possible to specify stop sequences for each token

--- get_completion/handler.py
+++ get_completion/handler.py
@@ -4,7 +4,7 @@ import os
 import requests
 import pymongo
 
-def huggingface(model_key, prompt: str, temperature=0.7, max_tokens=120):
+def huggingface(model_key, prompt: str, temperature=0.7, max_tokens=120, stop=None):
 	API_URL = "https://api-inference.huggingface.co/models/" + model_key # "bigscience/bloom"
 	response = requests.post(API_URL, headers={
 		"Authorization": "Bearer " + os.environ['HUGGINGFACE_API_KEY'],
@@ -14,6 +14,7 @@ def huggingface(model_key, prompt: str, temperature=0.7, max_tokens=120):
 			"max_length": max_tokens,
 			"temperature": temperature,
 			"return_full_text": False,
+			"stop": stop,
 		}
 	})
 	j = response.json()
@@ -21,8 +22,8 @@ def huggingface(model_key, prompt: str, temperature=0.7, max_tokens=120):
 		j = j[0]
 	return j['generated_text']
 
-def openai(model_key, prompt: str, temperature=0.7, max_tokens=120) -> str:
-	response = requests.post('https://api.openai.com/v1/completions', json={
+def openai(model_key, prompt: str, temperature=0.7, max_tokens=120, stop=None) -> str:
+	body = {
 		'model': model_key,
 		'prompt': prompt,
 		'temperature': temperature,
@@ -30,21 +31,23 @@ def openai(model_key, prompt: str, temperature=0.7, max_tokens=120) -> str:
 		'top_p': 1,
 		'frequency_penalty': 0,
 		'presence_penalty': 0,
-	}, headers={
+		'stop': stop,
+	}
+	headers = {
 		'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY'],
 		'Content-Type': 'application/json',
-	})
+	}
+	response = requests.post('https://api.openai.com/v1/completions', json=body, headers=headers)
 	response = response.json()
+
+	print("POST https://api.openai.com/v1/completions")
+	print(f"{body=} {headers=}")
+	print(f"{response=}")
 
 	if 'choices' not in response:
 		raise ValueError("Invalid response from OpenAI: " + str(response))
 
 	return response['choices'][0]['text']
-
-models = {
-	'gpt3': openai,
-	'gptj': functools.partial(huggingface, 'EleutherAI/gpt-j-6B'),
-}
 
 client = pymongo.MongoClient(os.environ['MONGO_URI'])
 db = client['test']
@@ -52,6 +55,7 @@ interactions = db['interactions']
 tokens = db['tokens']
 
 def r(code, body):
+	print("Response {} with body {}".format(code, json.dumps(body)))
 	return {
 		'statusCode': code,
 		'body': json.dumps(body),
@@ -66,6 +70,8 @@ def lambda_handler(event, context):
 		prompt = body.get("prompt")
 		token = body.get("token")
 
+		print("Received event with body:", event.get('body'))
+
 		if prompt is None or token is None:
 			return r(400, {"error": "Invalid request: Missing 'prompt' or 'token' parameter"})
 
@@ -78,12 +84,18 @@ def lambda_handler(event, context):
 		max_tokens: int = token_data.get('max_tokens', 120)
 		method: str = token_data.get('method', 'openai:text-davinci-003')
 
+		temperature = token_data.get('temperature', 0.7)
+		stop = token_data.get('stop', None)
+
 		if method.startswith("hf:"):
 			model = functools.partial(huggingface, method[3:])
 		elif method.startswith("openai:"):
 			model = functools.partial(openai, method[7:])
 
-		completion = model(prompt, temperature=0.7, max_tokens=max_tokens)
+		completion = model(prompt, temperature=temperature, max_tokens=max_tokens, stop=stop)
+
+		if not completion:
+			print(f"WARNING: No completion; {prompt=} {model=} {max_tokens=}")
 
 		return r(200, {"completion": completion})
 		
